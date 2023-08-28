f92524021a29163fbdfc3403ead6e2bcd22f5fc5
Harshitha M <harshitha8646@gmail.com>
2023-08-28 19:13:38 +0530

    First Commit

---
+++ home/footer.php
@@ -0,0 +1,27 @@
+<!-- copyright and contactUs fixed footer -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.footer {
+    position: fixed;
+    width: 100%;
+    text-align: center;
+    
+    bottom: 0;
+    background-color: #2ab7ca;
+    color: white;
+}
+</style>
+</head>
+<body>
+<footer class="footer">
+
+  <p style="margin-top: 4px;margin-bottom:-10px">Copyright © 2021 Designed By Harshitha M and Ksheeraja</p><br>
+  <a href="mailto:1b019cs042.harshitha@brindavancollege.com" style="color:brown">ContactUs</a>
+
+</footer>
+
+</body>
+</html>
